e13f8703ec7d8d7178a46074251bad9a0a23cbb5
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-11-11 10:24:07 +0100

    Add project files.

---
+++ kata_oscar/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace kata_oscar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
